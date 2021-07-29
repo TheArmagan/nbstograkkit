d9dd790c7875b5f8547825cb65de275c1f3794c1
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2021-07-29 23:34:38 +0300

    finally working
    much thanks to @CanCodes can helped me to compress file size
    
    Co-Authored-By: Can <32599057+cancodes@users.noreply.github.com>

--- index.js
+++ index.js
@@ -6,26 +6,38 @@ let song = NBS.loadSong(path.resolve(__dirname, process.argv[2]));
 
 let layers = Object.values(song.layers);
 
-let result = [];
-let sleepDur = song.tempo/1000;
-
+let data = [];
 for (let noteIndex = 0; noteIndex < song.length + 1; noteIndex++) {
-  let notePack = [];
-  let sleeps = 0;
-
   for (let lIndex = 0; lIndex < layers.length; lIndex++) {
     let layer = layers[lIndex];
     let note = layer.notes[noteIndex];
 
     if (note) {
-      notePack.push([note.instrument, Number(getClicks(note.pitch))])
+      data.push([note.instrument, Number(getClicks(note.pitch))])
     } else {
-      sleeps += sleepDur;
+      data.push(0)
     }
   }
-  if (notePack.length > 0) result.push([1, notePack]);
-  if (sleeps > 0) result.push([0, Number(sleeps.toFixed(2))])
-
 }
-console.log(JSON.stringify(result))
 
+let data2 = [];
+let count = 0;
+for (let i = 0; i < data.length; i++) {
+  const el = data[i];
+  if (typeof el === "object") {
+    if (count > 0) data2.push(count);
+    data2.push(el);
+    count = 0;
+  } else {
+    count += 1;
+  }
+}
+if (count > 0) data2.push(count);
+
+
+let result = {
+  tempo: song.tempo,
+  data: data2
+};
+
+console.log(JSON.stringify(result));
